34f2febada58a9010b41298518cd3e7073adef2b
Leon Munene <leonmunene@gmail.com>
2025-09-30 10:03:46 +0300

    time2

--- backend/resumeforge_backend/settings.py
+++ backend/resumeforge_backend/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'graphene_django',
-    'django_celery_results',
     'api.apps.ApiConfig',
     'rest_framework',
     'rest_framework_simplejwt',
