5fbab943e334daa9d1ffe05ceaa6c6ec0433973c
Leon Munene <leonmunene@gmail.com>
2025-10-06 08:19:43 +0300

    migrations and apps installed

--- backend/resumeforge_backend/settings.py
+++ backend/resumeforge_backend/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'api.apps.ApiConfig',
     'rest_framework',
     'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
     'graphql_jwt',
     "corsheaders",
 ]
